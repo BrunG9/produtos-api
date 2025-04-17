6f0805e3fbedaed11aeda98be699c240e9ebb428
Bruno Grassi <brunoograssi@hotmail.com>
2025-04-17 00:37:22 -0300

    Create project: API de Produtos

---
+++ src/test/java/bruno/project/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.project.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
