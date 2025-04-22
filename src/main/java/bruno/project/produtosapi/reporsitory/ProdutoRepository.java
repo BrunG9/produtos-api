a89d50915152ef43a249d7f623f20802ecf8931b
Bruno Grassi <brunoograssi@hotmail.com>
2025-04-22 16:16:39 -0300

    Adição do método findByName

--- src/main/java/bruno/project/produtosapi/reporsitory/ProdutoRepository.java
+++ src/main/java/bruno/project/produtosapi/reporsitory/ProdutoRepository.java
@@ -3,5 +3,8 @@ package bruno.project.produtosapi.reporsitory;
 import bruno.project.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByName(String name);
 }
